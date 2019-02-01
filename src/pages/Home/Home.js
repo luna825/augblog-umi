fd522c19cd3068e622227e46e1a4104b98b6c9a7
luna825 <luna825@qq.com>
2019-02-01 20:33:26 +0800

    首页个人信息

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Layout } from 'antd';
+import { Layout, Divider, Icon, Card } from 'antd';
 import { connect } from 'dva';
 import GridContent from '@/components/PageHeaderWrapper/GridContent';
 import PostList from './PostList';
@@ -32,8 +32,23 @@ class Home extends PureComponent {
         <Content>
           <PostList articles={articles} loading={loading} handleInfiniteOnLoad={this.onLoadMore} />
         </Content>
-        <Sider className={styles.sider} breakpoint="lg" collapsedWidth={0} trigger={null}>
-          sider
+        <Sider width={300} className={styles.sider} breakpoint="lg" collapsedWidth={0} trigger={null}>
+          <Card bordered={false}>
+            <div className={styles.avatarHolder}>
+              <img alt="" src="https://avatars0.githubusercontent.com/u/10859713?s=460&v=4" />
+              <div className={styles.name}>August</div>
+            </div>
+            <div className={styles.detail}>
+              <p>欢迎来到我的博客!</p>
+              <p>我是一名计算机爱好者，目前居住于重庆</p>
+              <p>谢谢你的访问。</p>
+            </div>
+            <Divider dashed />
+            <div className={styles.link}>
+              <div> <Icon type='mail' /> luna825@qq.com </div>
+              <div> <Icon type='github' /> <a rel="noopener noreferrer" target="_blank" href="https://github.com/luna825">Github </a></div>
+            </div>
+          </Card>
         </Sider>
       </GridContent>
     );
